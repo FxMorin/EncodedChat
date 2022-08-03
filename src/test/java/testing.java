ea02792f06bf6c33e365ab462e734961ea1dd155
FX - PR0CESS <28154542+fmorin@users.noreply.github.com>
2022-08-03 19:31:52 -0400

    Setup actual testing

--- src/test/java/testing.java
+++ src/test/java/testing.java
@@ -1,32 +1,35 @@
+import ca.fxco.encodedchat.EncodedChat;
 import ca.fxco.encodedchat.encodingSets.*;
 
 public class testing {
 
+    private static String[] testStrings = new String[]{
+            "Wow you're pretty smart!",
+            "this is a magical test",
+            "Fx was here",
+            "1234 I like to encode"
+    };
+
     public static void main(String[] args) {
-        String encodeMe = "Wow you're pretty smart, congrats!";
-
-        SeedEncodingSet seedEncoding = new SeedEncodingSet();
-        Base32EncodingSet base32Encoding = new Base32EncodingSet();
-        Base64EncodingSet base64Encoding = new Base64EncodingSet();
-        Rot13EncodingSet rot13Encoding = new Rot13EncodingSet();
-        CaesarEncodingSet caesarEncoding = new CaesarEncodingSet();
-
-        String seedEncoded = seedEncoding.encode(encodeMe);
-        String base32Encoded = base32Encoding.encode(encodeMe);
-        String base64Encoded = base64Encoding.encode(encodeMe);
-        String rot13Encoded = rot13Encoding.encode(encodeMe);
-        String caesarEncoded = caesarEncoding.encode(encodeMe);
-
-        String seedDecoded = seedEncoding.decode(seedEncoded);
-        String base32Decoded = base32Encoding.decode(base32Encoded);
-        String base64Decoded = base64Encoding.decode(base64Encoded);
-        String rot13Decoded = rot13Encoding.decode(rot13Encoded);
-        String caesarDecoded = caesarEncoding.decode(caesarEncoded);
-
-        System.out.println("seed - encoded: "+seedEncoded+ " decoded: "+seedDecoded);
-        System.out.println("base32 - encoded: "+base32Encoded+ " decoded: "+base32Decoded);
-        System.out.println("base64 - encoded: "+base64Encoded+ " decoded: "+base64Decoded);
-        System.out.println("rot13 - encoded: "+rot13Encoded+ " decoded: "+rot13Decoded);
-        System.out.println("caesar - encoded: "+caesarEncoded+ " decoded: "+caesarDecoded);
+        new EncodedChat().onInitializeClient();
+        for (String encodeMe : testStrings) {
+            if (encodeMe.length() > 36) continue;
+            System.out.println("Testing String: `"+encodeMe+"`");
+            System.out.println(" set Name |autoDetect|canDecode|                  encoded                   |                  decoded");
+            for (EncodingSet set : EncodedChat.ENCODING_SETS.values()) {
+                if (set.canEncode(encodeMe)) {
+                    String encoded = set.encode(encodeMe);
+                    boolean hasEncoding = set.hasEncoding(encoded);
+                    String decoded = hasEncoding ? set.decode(encoded) : "";
+                    System.out.printf("%10s|%10b|%9b|%44s|%s%n",
+                            set.getId(),
+                            set.canAutomaticallyDetect(),
+                            hasEncoding,
+                            encoded,
+                            decoded
+                    );
+                }
+            }
+        }
     }
 }
